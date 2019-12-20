a27af45699ce1a04a05e1972b9b92a836789e264
Mauro Doneda <mauro.doneda@gmail.com>
2019-12-20 19:52:14 +0000

    Changed the server to SQL Server

---
+++ Data/WhatToDoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WhatToDo.Models;
+
+namespace WhatToDo.Data
+{
+    public class WhatToDoContext : DbContext
+    {
+        public WhatToDoContext (DbContextOptions<WhatToDoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WhatToDo.Models.ToDo> ToDo { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using WhatToDo.Data;
 
 namespace WhatToDo
 {
@@ -24,6 +26,9 @@ namespace WhatToDo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<WhatToDoContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WhatToDoContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
